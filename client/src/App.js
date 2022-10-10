e0512b9d7d161b12c8a64d347114a759cb6ab6cb
DorisLiu <dorisliu113@gmail.com>
2022-10-10 12:09:54 +1100

    first commit for basic structure

---
+++ client/src/App.js
@@ -0,0 +1,21 @@
+import React from 'react';
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/Navbar';
+
+function App() {
+  return (
+    <Router>
+      <>
+        <Navbar />
+        <Routes>
+          <Route 
+            path='*'
+            element={<h1 className='display-2'>Wrong page!</h1>}
+          />
+        </Routes>
+      </>
+    </Router>
+  );
+}
+
+export default App;
