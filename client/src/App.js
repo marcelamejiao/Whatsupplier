232d05a57057dd9515651dd50af5d1dab0f725f7
DorisLiu <dorisliu113@gmail.com>
2022-10-17 20:50:07 +1100

    set up the routes in App.js

--- client/src/App.js
+++ client/src/App.js
@@ -1,15 +1,23 @@
 import React from 'react';
 import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
-import { 
-        ApolloClient,
-        ApolloProvider,
-        InMemoryCache,
-        createHttpLink,
+import {
+  ApolloClient,
+  ApolloProvider,
+  InMemoryCache,
+  createHttpLink,
 } from '@apollo/client';
 import { setContext } from '@apollo/client/link/context';
-import SearchBooks from './pages/SearchBooks';
-import SavedBooks from './pages/SavedBooks';
+import LandingPage from './pages/LadingPage';
+import Login from './pages/Login';
+import Signup from './pages/Signup';
+import Home from './pages/Home';
+import MaterialsList from './pages/MaterialsList'
+import SingleMaterial from './pages/MaterialsList'
+import SuppliersList from './pages/SuppliersList';
+import SingleSupplier from './pages/OneSupplier';
+import Production from './pages/Production'
 import Navbar from './components/Navbar';
+import Footer from './components/Footer';
 
 // Construct our main GraphQL API endpoint
 const httpLink = createHttpLink({
@@ -41,20 +49,52 @@ function App() {
       <Router>
         <>
           <Navbar />
-          <Routes>
-            <Route 
-              path='/' 
-              element={<SearchBooks />} 
-            />
-            <Route 
-              path='/saved' 
-              element={<SavedBooks />} 
-            />
-            <Route 
-              path='*'
-              element={<h1 className='display-2'>Wrong page!</h1>}
-            />
-          </Routes>
+          <div>
+            <Routes>
+              <Route
+                path='/'
+                element={<LandingPage />}
+              />
+              <Route
+                path="/login"
+                element={<Login />}
+              />
+              <Route
+                path="/signup"
+                element={<Signup />}
+              />
+              <Route
+                path="/me"
+                element={<Home />}
+              />
+              <Route
+                path="/inventory"
+                element={<MaterialsList />}
+              />
+              <Route
+                path="/inventory/:materialName"
+                element={<SingleMaterial />}
+              />
+              <Route
+                path="/production"
+                element={<Production />}
+              />
+              <Route
+                path="/suppliers"
+                element={<SuppliersList />}
+              />
+              <Route
+                path="/suppliers/:supplierName"
+                element={<SingleSupplier />}
+              />
+              <Route
+                path='*'
+                element={<h1 className='display-2'>Wrong page!</h1>}
+              />
+            </Routes>
+          </div>
+
+          <Footer />
         </>
       </Router>
     </ApolloProvider>
---
+++ client/src/components/Footer.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+
+const Footer = () => {
+
+    return (
+        <div>
+            Footer
+        </div>
+    );
+};
+
+export default Footer;
