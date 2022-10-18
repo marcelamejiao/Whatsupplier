d8d183039611882322de9aae2fc12b942464ff89
Marcela Mejia <mejiao24@hotmail.com>
2022-10-18 16:31:42 +0700

    Turn off unused mutations

--- client/src/utils/mutations.js
+++ client/src/utils/mutations.js
@@ -24,70 +24,70 @@ export const ADD_USER = gql`
   }
 `;
 
-export const ADD_SUPPLIER = gql`
-  mutation addSupplier($name: String!, $email: String!, $address: String!, $phone: String!) {
-    addSupplier(name: $name, email: $email, address: $address, phone: $phone) {
-            name
-            email
-            address
-            phone
-        }
-    }
-`;
+// export const ADD_SUPPLIER = gql`
+//   mutation addSupplier($name: String!, $email: String!, $address: String!, $phone: String!) {
+//     addSupplier(name: $name, email: $email, address: $address, phone: $phone) {
+//             name
+//             email
+//             address
+//             phone
+//         }
+//     }
+// `;
 
-export const UPDATE_SUPPLIER = gql`
-    mutation UpdateSupplier($_id: ID! $name: String!, $email: String!, $address: String!, $phone: String!) {
-        updateSupplier(_id: $_id, name: $name, email: $email, address: $address, phone: $phone) {
-            _id
-            name
-            email
-            address
-            phone
-        }
-    }
-`;
+// export const UPDATE_SUPPLIER = gql`
+//     mutation UpdateSupplier($_id: ID! $name: String!, $email: String!, $address: String!, $phone: String!) {
+//         updateSupplier(_id: $_id, name: $name, email: $email, address: $address, phone: $phone) {
+//             _id
+//             name
+//             email
+//             address
+//             phone
+//         }
+//     }
+// `;
 
-export const DELETE_SUPPLIER = gql`
-    mutation deleteSupplier($_id: _id) {
-        deleteSupplier(_id: $_id) {
-            _id
-            name
-            email
-            address
-            phone 
-        }
-    }
-`;
+// export const DELETE_SUPPLIER = gql`
+//     mutation deleteSupplier($_id: _id) {
+//         deleteSupplier(_id: $_id) {
+//             _id
+//             name
+//             email
+//             address
+//             phone 
+//         }
+//     }
+// `;
 
-export const UPDATE_USER_MATERIAL = gql`
-    mutation updateUserMaterial($id: ID!, $name: String!, $stock: Int!, $safetyStock: Int!, $anticipatedDemand: Int! {
-        updateUserMaterial(_id: $id, name: $name, stock: $stock, safetyStock: $safetyStock, anticipatedDemand: $anticipatedDemand) {
-            userMaterials {
-                material {
-                  name
-                }
-                stock
-                safetyStock
-                anticipatedDemand
-            }
+// export const UPDATE_USER_MATERIAL = gql`
+//     mutation updateUserMaterial($id: ID!, $name: String!, $stock: Int!, $safetyStock: Int!, $anticipatedDemand: Int! {
+//         updateUserMaterial(_id: $id, name: $name, stock: $stock, safetyStock: $safetyStock, anticipatedDemand: $anticipatedDemand) {
+//             userMaterials {
+//                 material {
+//                   name
+//                 }
+//                 stock
+//                 safetyStock
+//                 anticipatedDemand
+//             }
             
-        }
-    }
-`;
+//         }
+//     }
+// `;
 
-export const UPDATE_SUPPLIER_MATERIAL = gql`
-    mutation updateSupplierMaterial($id: ID!, $name: String!, $stock: Int!, $safetyStock: Int!, $anticipatedDemand: Int!) {
-        updateSupplierMaterial(_id: $id, materialId: $materialId, cost: $cost, leadTime: $leadTime) {
-            _id
-            name
-            supplierMaterials{
-                material{
-                    materialId
-                }
-                cost
-                leadTime
-            }
-        }
-    }
-`;
+// export const UPDATE_SUPPLIER_MATERIAL = gql`
+//     mutation updateSupplierMaterial($id: ID!, $name: String!, $stock: Int!, $safetyStock: Int!, $anticipatedDemand: Int!) {
+//         updateSupplierMaterial(_id: $id, materialId: $materialId, cost: $cost, leadTime: $leadTime) {
+//             _id
+//             name
+//             supplierMaterials{
+//                 material{
+//                     materialId
+//                 }
+//                 cost
+//                 leadTime
+//             }
+//         }
+//     }
+// `;
 
