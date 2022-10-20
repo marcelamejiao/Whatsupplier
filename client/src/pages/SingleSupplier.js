84311c7fe2cfdf3bcc549075f7f80194d82d8f48
Marcela Mejia <mejiao24@hotmail.com>
2022-10-20 12:10:02 +0700

    Show supplier information on page

--- client/src/pages/SingleSupplier.js
+++ client/src/pages/SingleSupplier.js
@@ -8,21 +8,46 @@ import { UPDATE_SUPPLIER, UPDATE_SUPPLIER_MATERIAL, DELETE_SUPPLIER} from '../ut
 
 function SingleSupplier () {
   const { supplierId } = useParams();
+
   console.log(supplierId);
 
   // const [updateSupplier] = useMutation(UPDATE_SUPPLIER);
   // const [updateSupplierMaterial] = useMutation(UPDATE_SUPPLIER_MATERIAL);
   // const [deleteSupplier] = useMutation(DELETE_SUPPLIER);
 
-  // const { loading, error, data } = useQuery(QUERY_SINGLE_SUPPLIER);
+  const { loading, error, data } = useQuery(QUERY_SINGLE_SUPPLIER, {
+    variables: {
+      _id: supplierId,
+    }
+  });
 
-  // if (loading) {
-  //   return 'Loading...';
-  // }
+  if (loading) {
+    return 'Loading...';
+  }
 
-  // if (error) {
-  //   return `Error! ${error.message}`;
-  // }
+  if (error) {
+    return `Error! ${error.message}`;
+  }
+
+  const { supplier } = data;
+  console.log(supplier);
+
+  return (
+    <div className="card" style={{width: '18rem'}}>
+      <div className="card-body">
+      <h5 className="card-title">{supplier.name}</h5>
+      </div>
+      <ul className="list-group list-group-flush">
+        <li className="list-group-item">{supplier.address}</li>
+        <li className="list-group-item">{supplier.phone}</li>
+        <li className="list-group-item">{supplier.email}</li>
+      </ul>
+      <div className="card-body">
+        <a href="#" className="btn btn-primary">Modify</a>
+        <a href="#" className="btn btn-primary">Delete</a>
+      </div>
+    </div>
+  );
 
   // const { supplier } = data.supplier;
   // // const { materials } = data;
@@ -30,24 +55,6 @@ function SingleSupplier () {
   // // const materialList = materials.map(function(material){
   // //   return <option scope="row">{material.name}</option>
   // // });
-
-  // const supplierInfoCard = showCard ?
-  // (
-  //   <div class="card" style="width: 18rem;">
-  //       <div class="card-body">
-  //       <h5 class="card-title">{ supplier }</h5>
-  //     </div>
-  //     <ul class="list-group list-group-flush">
-  //       <li class="list-group-item"></li>
-  //       <li class="list-group-item"></li>
-  //       <li class="list-group-item"></li>
-  //     </ul>
-  //     <div class="card-body">
-  //       <a href="#" className="btn btn-primary">Modify</a>
-  //       <a href="#" className="btn btn-primary">Delete</a>
-  //     </div>
-  //   </div>
-  // ) : '';
 }
 
 export default SingleSupplier;
