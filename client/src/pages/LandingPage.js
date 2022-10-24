96eda4b6e354615ad1369229222641721e27af30
Marcela Mejia <mejiao24@hotmail.com>
2022-10-24 15:58:18 +0700

    Modify Landing Page text to follow the new README modifications

--- client/src/pages/LandingPage.js
+++ client/src/pages/LandingPage.js
@@ -7,17 +7,17 @@ function LandingPage({ currentPage, handlePageChange }) {
                 <ImageContainer>
                     <p>
                         WhatSupplier provides a tool for organizing and optimizing the process of ordering, storing, and using a company’s inventory.
-                        WhatSupplier also guides you with the selection process of choosing the supplier's lowest cost.
+                        WhatSupplier selects the supplier with the lowest cost based on a specific material.
                     </p>
                 </ImageContainer>
                 <FooterContainer>
                     <p>
-                        In the Supply Chain process the factory purchases the raw materials from diverse suppliers.
-                        Using the software the supplier’s manager can choose the best offer in price without sacrificing quality when needed to place a raw materials order.
+                        In the Supply Chain process the factory purchases the materials from diverse suppliers. 
+                        Using WhatSupplier the manager can choose the best offer in price when placing  an order.
                     </p>
                     <p>
-                        WhatSupplier will use a function to decide which supplier offers the lowest cost to place an order. 
-                        Also will use a function to determine the quantity in inventory of any raw material to proceed with placing a new order.
+                        WhatSupplier uses a function to decide which supplier offers the lowest cost to place an order.
+                        WhatSupplier uses a function to determine when an order needs to be placed because stocks are running too low. This function is called ROP “Reorder Point’. 
                     </p>
                 </FooterContainer>
         </MainPageContainer>
